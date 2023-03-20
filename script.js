1542c74a239c0583018899c51a7912fe5d34b08f
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-20 15:23:35 -0700

    added two new functions

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//creating the variables 
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+
+
 
 // creating the event for when the button is clicked 
 startButton.addEventListener("click", startGame)
@@ -13,6 +14,30 @@ nextButton.addEventListener('click', () => {
 //this starts the game and hides the first box
 function startGame() {
     console.log('started')
+    //this allows after you select the start game itll hide the box
     startButton.classList.add('hide')
+    //making the question container appear
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+
+  }
+  function setNextQuestion(){
+
+  }
+  function selectAnswer(){
     
   }
+  //making the questions 
+  const questions = [
+    {
+        question: 'What is 2+2?',
+        answer: [
+            {text: '4', corect: true},
+            {text: '1', corect: false},
+            {text: '3', corect: false},
+            {text: '5', corect: false}
+
+        ]
+    }
+
+  ]
