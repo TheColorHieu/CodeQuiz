d9b66e7a4fccd9fc6688b2951aa8681cf48ef117
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-20 22:58:27 -0700

    added questions and buttons to function

--- script.js
+++ script.js
@@ -1,32 +1,80 @@
 //creating the variables 
 const startButton = document.getElementById('start-btn')
+const nexttButton = document.getElementById('next-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
 
+let  shuffledQuestions, currentQuestionIndex
 
 // creating the event for when the button is clicked 
-startButton.addEventListener("click", startGame)
+startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
 })
-//this starts the game and hides the first box
-function startGame() {
-    console.log('started')
+
+  //this starts the game and hides the first box
+  function startGame() {
     //this allows after you select the start game itll hide the box
     startButton.classList.add('hide')
-    //making the question container appear
+    //were shuffling questions with this next line of code 
+    // shuffledQuestions = questions.sort(() => Math.random() - .5)
+    //the first question
+    currentQuestionIndex = 0
+    //were shuffling questions with this next line of code 
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
-
   }
+  //function to set next question
   function setNextQuestion(){
-
-  }
-  function selectAnswer(){
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
     
   }
+  //function to grab the question
+  function showQuestion(question){
+    questionElement.innerText = question.question
+    //appending the answers to the buttons 
+    question.answer.forEach(answer => {
+        const button = document.createElement('button')
+        button.innedText = answer.text
+        button.classList.add('btn')
+        //setting a condition to let us move on if true
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+
+
+    })
+    //reseting the questions
+    function resetState() {
+      clearStatusClass(document.body)
+      nextButton.classList.add('hide')
+      while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+      }
+    }
+
+    //creating the function to pick the correct class
+    function selectAnswer(e) {
+      const selectedButton = e.target
+      const correct = selectedButton.dataset.correct
+      setStatusClass(document.body, correct)
+      Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+      })
+      if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+      } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+      }
+    }
   //making the questions 
   const questions = [
     {
@@ -41,3 +89,4 @@ function startGame() {
     }
 
   ]
+}
